45cfc74355cad6b00c74adc26658b359e7926ee6
dashu6101 <dashu.web@gmail.com>
2026-06-08 08:55:41 -0400

    change in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,6 +18,10 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
