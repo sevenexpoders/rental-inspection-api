6933ea34c083ecad8e728275ff6dc9f50fb0e4df
dashu6101 <dashu.web@gmail.com>
2026-07-15 02:18:24 -0400

    feat: add logging for AWS S3 presigned URL generation

--- src/common/helpers/s3-presigned.helper.ts
+++ src/common/helpers/s3-presigned.helper.ts
@@ -4,6 +4,13 @@ import { s3 } from '../../config/s3.config';
   
 export async function getPresignedS3Url(key: string, expiresInSeconds = 172800 , bucketName :string): Promise<string> {
  
+  console.log({
+    region: process.env.AWS_REGION,
+    endpoint: process.env.AWS_ENDPOINT,
+    bucket: process.env.AWS_BUCKET_NAME,
+    accessKeyLength: process.env.AWS_ACCESS_KEY?.length,
+    secretKeyLength: process.env.AWS_SECRET_KEY?.length,
+  });
   if (!bucketName) {
     throw new Error('AWS_BUCKET_NAME_UNDEFINED:');
   }
